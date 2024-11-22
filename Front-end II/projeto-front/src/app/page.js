655906221505769831c66fbe631c25a7bff9206a
brendavic0902 <brendavitoriagomesdematos7@gmail.com>
2024-11-22 13:54:50 -0400

    commit inicial

--- Front-end II/projeto-front/src/app/page.js
+++ Front-end II/projeto-front/src/app/page.js
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Meu título</h1>
-      <Image className={styles.img}src='/images/dany.jpg' alt="Imagem da mae dos dragoes" width={400} height={400} / >
+      <h1>THE WEEKND</h1>
+      <Image className={styles.img}src='/images/dany.jpg' alt="the weeknd" width={400} height={400} / >
     </div>
   );
 }
