ed50889939bc46fce7ff0b413f26614062dc3eca
brendavic0902 <brendavitoriagomesdematos7@gmail.com>
2024-11-26 17:24:14 -0400

    26.11

--- Front-end II/projeto-front/src/app/page.js
+++ Front-end II/projeto-front/src/app/page.js
@@ -4,7 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>THE WEEKND</h1>
+      <h1 className={styles.h1}>THE WEEKND</h1>
+      <p className={styles.p}>Conhecido por sua versatilidade sonora e lirismo sombrio, suas músicas exploram temas de escapismo, romance e melancolia, e são frequentemente inspiradas em experiências pessoais. 
+        Ele ajudou a expandir a paleta musical do R&B, ao incorporar influências indie e de música eletrônica; seus trabalhos iniciais foram classificados como sendo R&B alternativo.</p>
       <Image className={styles.img}src='/images/dany.jpg' alt="the weeknd" width={400} height={400} / >
     </div>
   );
--- Front-end II/projeto-front/src/app/sobre/page.js
+++ Front-end II/projeto-front/src/app/sobre/page.js
@@ -1,11 +1,14 @@
-const Sobre = () => {
+import styles from "./sobre.module.css";
+import Image from "next/image";
+export default function Sobre() {
     return (
         <main>
-            <h1>Página Sobre</h1>
-            <p>meu paragrafo</p>
+            <p className={styles.para}>Abel Makkonen Tesfaye, mais conhecido por seu nome artístico The Weeknd, é um cantor, compositor, ator e produtor musical canadense.</p>
+            <Image className={styles.img_sobre} src='/images/theweeknd.jpg' alt="gfg" width={450} height={500} />
+            <p className={styles.p}> Em 2010, ele começou a disponibilizar anonimamente várias canções no YouTube sob o nome "The Weeknd" e, em 2011, lançou as aclamadas mixtapes House of Balloons, Thursday e Echoes of Silence. 
+                Suas mixtapes foram posteriormente remasterizadas e relançadas na coletânea Trilogy (2012). </p>
         </main>
         
-    )
+         
+);
 }
-
-export default Sobre;
